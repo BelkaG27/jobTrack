dcaf0011ac41ccf6848a57b0d2a4bcb2f7c33161
Belkacem <belkag7@gmail.com>
2026-09-09 03:27:32 +0200

    added a handler for when an argument for a request isn't valid

--- src/main/java/JobTrack/Exceptions/GlobalExceptionHandler.java
+++ src/main/java/JobTrack/Exceptions/GlobalExceptionHandler.java
@@ -4,6 +4,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+
+
 
 @RestControllerAdvice 
 public class GlobalExceptionHandler {
@@ -12,5 +17,18 @@ public class GlobalExceptionHandler {
     public ResponseEntity<String> handleCandidatureNotFound(CandidatureNotFoundException exception){
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
     }
-    
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String,String>> handleValidationErrors(MethodArgumentNotValidException exception){
+        
+        Map<String,String> errors = new HashMap<>();
+
+        exception.getBindingResult()
+                .getFieldErrors()
+                .forEach(error->
+                    errors.put(error.getField(), error.getDefaultMessage())
+                );
+        
+        return ResponseEntity.badRequest().body(errors);
+    }
 }
